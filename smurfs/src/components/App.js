9aaa962337b5dbf1d6aa5be6021c6d46fda7ff72
allisonkydy <allisnelly@gmail.com>
2019-09-13 11:07:59 -0500

    smurfs displaying in ui

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,6 +6,7 @@ import { getSmurfs } from '../actions';
 
 function App() {
   const dispatch = useDispatch();
+  const smurfs = useSelector(state => state.smurfs);
   const isFetching = useSelector(state => state.isFetching);
   const error = useSelector(state => state.error);
 
@@ -19,10 +20,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
+      <h1>smurf village</h1>
+      {smurfs.map(smurf => {
+        return (
+          <div>
+            <h2>{smurf.name}</h2>
+            <p>Age: {smurf.age} years</p>
+            <p>Height: {smurf.height}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
